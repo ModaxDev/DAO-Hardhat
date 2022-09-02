5a645c3e1ab5a48b4685fbfe3f7b5d5b1669f15c
Clément Verhille <61698516+ModaxDev@users.noreply.github.com>
2022-09-03 00:42:54 +0200

    Initial commit

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x9A620235Ea806094eD816f22aC8Fdc9FCa9cb1eB";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
